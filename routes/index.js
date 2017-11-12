22d09fc486677ce36dc036a81bcd2a0ff08f45b3
Abhi <you@example.com>
2017-11-11 20:21:47 -0800

    first commit

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,24 @@ exports.index = function(req, res){
 
 
 exports.incoming1 = function(req, res){
-	console.log(req.query.text);
+	var apiai = require('apiai');
+	 
+	var app = apiai("93e838129c1f441d824b26677b715f93");
+	console.log(req.query.text); 
+	var request_api = app.textRequest(req.query.text, {
+	    sessionId: '001'
+	});
+	 
+	request_api.on('response', function(response) {
+	    console.log("response from api.ai"+response);
+	});
+	 
+	request_api.on('error', function(error) {
+	    console.log(error);
+	});
+	 
+	request_api.end();
+	
+	
 	res.status(200).end();
 };
