5c70ce567f8c0bb0fac8839eac7c32e3c0d09dbe
Abhi <you@example.com>
2017-11-12 09:48:34 -0800

    first commit

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/incoming1', routes.incoming1);
+app.get('/sendPromo', routes.sendPromo);
 app.get('/users', user.list);
 
 
