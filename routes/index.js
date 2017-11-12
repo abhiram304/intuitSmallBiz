9ce80357634c22b90b230ea0f390cf0ef053493e
Abhi <you@example.com>
2017-11-11 19:32:13 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+	
+	var Nexmo = require('nexmo');
+
+	var nexmo = new Nexmo({
+	  apiKey:'51a228ab' ,
+	  apiSecret:'4331f47de69da867' ,
+	});
+
+	var from = '12016728822';
+	var to = '15102039956';
+	var text = 'hello';
+
+	console.log(nexmo.message.sendSms(from, to, text));
+  res.render('index', { title: 'Express' });
+};
+
+
+
+exports.incoming1 = function(req, res){
+	console.log(req.query);
+};
